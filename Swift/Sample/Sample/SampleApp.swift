0276dfe84b15b6627ac054e1053b06d1921baa5b
有川 泰浩 <hipgnosis_mini@arikawa-no-iMac.local>
2021-02-03 00:10:03 +0900

    ar-sample

---
+++ Swift/Sample/Sample/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  Sample
+//
+//  Created by 有川 泰浩 on 2021/01/19.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
